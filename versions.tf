eb69f650949614db0a219a327e3340a485ac1af7
Seth Erickson <sr.erickson@gmail.com>
2023-06-21 15:32:45 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud",
-      version = "= 1.27.2"
+      version = "= 1.40.0"
     }
   }
-  required_version = "= 1.0.1"
+  required_version = "= 1.4.6"
 }
